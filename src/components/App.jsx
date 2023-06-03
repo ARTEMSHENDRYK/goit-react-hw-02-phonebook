9584d01eab1594d2c0f7dd707e51eb4db6dc534f
ARTEMSHENDRYK <artemshendryk@gmail.com>
2023-06-03 18:34:44 +0300

    do css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,58 @@
 import React from "react";
+import { nanoid } from 'nanoid'
 import ContactForm from "./ContactForm/ContactForm";
-import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
 import css from "./App.module.css";
 
 class App extends React.Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  }
+  
+  handleSubmit = ({ name, number}) => {
+    if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase()))
+    {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    this.setState({ contacts: [...this.state.contacts, { id: nanoid(), name: name, number: number }] });
   }
 
-  render() {
+  handleFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  }
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
+  }
+
+  handleDelete = id => {
+    const { contacts } = this.state;
+    this.setState({ contacts: contacts.filter(contact => contact.id !== id) });
+  }
+
+  render() {  
     return (
       <div>
         <h1 className={css.title}>Phonebook</h1>
-        <ContactForm />
+        <ContactForm onAddContact={this.handleSubmit}/>
         <h2 className={css.title}>Contacts</h2>
-        <Filter />
-        <ContactList />
+        <Filter onFilter={this.handleFilter} />
+        {this.state.contacts.length > 0
+          ? (
+            <ContactList
+                contacts={this.filterContacts()} onDelete={this.handleDelete}/>
+          )
+          : (<p className={css.title}>There are no contacts.</p>)    
+        }
       </div>
     );
   };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,40 @@
-import PropTypes, { func } from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
+import ContactElement from "components/ContactElement/ContactElement";
+import css from "./ContactList.module.css";
 
-function ContactList() {
+class ContactList extends React.Component {
+  render() {
+    const { contacts, onDelete } = this.props;
+    return (
+      <div className={css.container}>
+        <ul>
+          {contacts.map(({ id, name, number }) => {
+            return (
+              <li className={css.list} key={id}>
+                <ContactElement
+                  id={id}
+                  name={name}
+                  number={number}
+                  onDelete={onDelete}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
 
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 }
 
 export default ContactList;
